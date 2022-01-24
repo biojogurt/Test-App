5017c19f2472b68183b1e0f5c647ce98432f455d
Polina Amelina <anathebeequeen@gmail.com>
2022-01-24 19:32:51 +0300

    пустой проект

---
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.testapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.testapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    companion object {
+        // Used to load the 'testapp' library on application startup.
+        init {
+            System.loadLibrary("testapp")
+        }
+    }
+}
