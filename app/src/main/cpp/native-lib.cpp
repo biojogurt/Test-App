a859b72860b1e0a1c203a9fda2e87ea859047350
Polina Amelina <anathebeequeen@gmail.com>
2022-01-25 19:50:30 +0300

    поправила код по всему проекту, в частности код вывода таблицы

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,33 +5,31 @@ extern "C" JNIEXPORT jobjectArray JNICALL
 Java_com_example_testapp_MainActivity_getPhonebook(JNIEnv *env, jobject)
 {
     srand(time(nullptr));
+    jclass phClass = env->FindClass("com/example/testapp/PhonebookEntry");
 
     // будет сгенерировано между 5 и 20 парами имя-номер
     int num_entries = rand() % 15 + 5;
-
-    jclass phClass = env->FindClass("com/example/testapp/PhonebookEntry");
     jobjectArray phArray = env->NewObjectArray(num_entries, phClass, nullptr);
-    jmethodID phInit = env->GetMethodID(phClass, "<init>",
-                                        "(Ljava/lang/String;Ljava/lang/String;)V");
 
     for (int j = 0; j < num_entries; ++j)
     {
         std::string name, number;
 
-        // генерирует фамилию - рандомный набор букв от 5 до 8 символов
-        name += rand() % 26 + 'A';
-        int name_length = rand() % 3 + 4;
+        // генерирует фамилию - рандомный набор букв от 8 до 12 символов
+        name += (char) (rand() % 26 + 'A');
+        int name_length = rand() % 4 + 7;
         for (int i = 0; i < name_length; ++i)
-            name += rand() % 26 + 'a';
+            name += (char) (rand() % 26 + 'a');
 
         // генерирует номер - рандомный набор цифр
         for (int i = 0; i < 11; ++i)
-            number += rand() % 10 + '0';
+            number += (char) (rand() % 10 + '0');
 
-        jobject phObj = env->NewObject(phClass, phInit,
-                                       env->NewStringUTF(name.c_str()),
-                                       env->NewStringUTF(number.c_str()));
-        env->SetObjectArrayElement(phArray, j, phObj);
+        env->SetObjectArrayElement(phArray, j, env->NewObject(phClass,
+                                                              env->GetMethodID(phClass, "<init>",
+                                                                               "(Ljava/lang/String;Ljava/lang/String;)V"),
+                                                              env->NewStringUTF(name.c_str()),
+                                                              env->NewStringUTF(number.c_str())));
     }
 
     return phArray;
--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -20,24 +20,37 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // получает телефонную книгу из c++ части
         val phonebook = getPhonebook()
 
         binding.textViewTopName.text = phonebook[0].name
         binding.textViewTopNumber.text = phonebook[0].number
+
+        // запоминает id последнего добавленного элемента чтобы прикреплять к нему последующий
         var previousNameViewId = binding.textViewTopName.id
         var previousNumberViewId = binding.textViewTopNumber.id
-        val padding =
-            TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5f, resources.displayMetrics)
-                .toInt()
+
+        // высчитывает внутренние границы элементов в dp
+        val padding = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            5f,
+            resources.displayMetrics
+        ).toInt()
 
         for (i in 1 until phonebook.size) {
+
             previousNameViewId = addTextView(
-                phonebook[i].name, padding,
-                previousNameViewId, true
+                phonebook[i].name,
+                padding,
+                previousNameViewId,
+                true
             )
+
             previousNumberViewId = addTextView(
-                phonebook[i].number, padding,
-                previousNumberViewId, false
+                phonebook[i].number,
+                padding,
+                previousNumberViewId,
+                false
             )
         }
     }
@@ -45,73 +58,59 @@ class MainActivity : AppCompatActivity() {
     private fun addTextView(text: String, padding: Int, previousId: Int, is_name: Boolean): Int {
 
         val tv = TextView(this)
-        tv.layoutParams = ConstraintLayout.LayoutParams(
-            ConstraintLayout.LayoutParams.MATCH_PARENT,
-            ConstraintLayout.LayoutParams.WRAP_CONTENT
-        )
-        if (is_name) binding.root.addView(tv) else binding.constraintLayoutNumber.addView(tv)
 
         tv.id = View.generateViewId()
         tv.text = text
         tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
         tv.setPadding(padding)
+        tv.layoutParams = ConstraintLayout.LayoutParams(
+            ConstraintLayout.LayoutParams.MATCH_PARENT,
+            ConstraintLayout.LayoutParams.WRAP_CONTENT
+        )
 
-        if (is_name) connectNameView(tv, previousId) else connectNumberView(tv, previousId)
+        connectTextView(
+            tv,
+            if (is_name) binding.root else binding.constraintLayoutNumber,
+            previousId,
+            is_name
+        )
+
         return tv.id
     }
 
-    private fun connectNameView(tv: TextView, previousId: Int) {
+    private fun connectTextView(tv: TextView, layout: ConstraintLayout, previousId: Int, is_name: Boolean) {
+
+        layout.addView(tv)
         val cs = ConstraintSet()
-        cs.clone(binding.root)
+        cs.clone(layout)
+
         cs.connect(
             tv.id,
-            ConstraintSet.START,
-            binding.root.id,
-            ConstraintSet.START
+            if (is_name) ConstraintSet.START else ConstraintSet.END,
+            layout.id,
+            if (is_name) ConstraintSet.START else ConstraintSet.END
         )
+
         cs.connect(
             tv.id,
-            ConstraintSet.END,
+            if (is_name) ConstraintSet.END else ConstraintSet.START,
             binding.guidelineCenter.id,
             ConstraintSet.START
         )
-        cs.connect(
-            tv.id,
-            ConstraintSet.TOP,
-            previousId,
-            ConstraintSet.BOTTOM
-        )
-        cs.applyTo(binding.root)
-    }
 
-    private fun connectNumberView(tv: TextView, previousId: Int) {
-        val cs = ConstraintSet()
-        cs.clone(binding.constraintLayoutNumber)
-        cs.connect(
-            tv.id,
-            ConstraintSet.END,
-            binding.constraintLayoutNumber.id,
-            ConstraintSet.END
-        )
-        cs.connect(
-            tv.id,
-            ConstraintSet.START,
-            binding.constraintLayoutNumber.id,
-            ConstraintSet.START
-        )
         cs.connect(
             tv.id,
             ConstraintSet.TOP,
             previousId,
             ConstraintSet.BOTTOM
         )
-        cs.applyTo(binding.constraintLayoutNumber)
+
+        cs.applyTo(layout)
     }
 
     private external fun getPhonebook(): Array<PhonebookEntry>
 
     companion object {
-        // Used to load the 'testapp' library on application startup.
         init {
             System.loadLibrary("testapp")
         }
