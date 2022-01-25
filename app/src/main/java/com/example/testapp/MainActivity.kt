8bd150d1dd0288b78c33380aad696adc88e14ce2
Polina Amelina <anathebeequeen@gmail.com>
2022-01-25 23:56:36 +0300

    вынесла данные телефонной книги в modelview, чтобы не пропадали при переходе

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -5,27 +5,30 @@ import android.os.Bundle
 import android.util.TypedValue
 import android.view.View
 import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.setPadding
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.testapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    // получает телефонную книгу из c++ части и сохраняет отдельно, чтобы хранить информацию на протяжении всей жизни приложения
+    private val mainViewModel: MainViewModel by viewModels { MainViewModelFactory(getPhonebook()) }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // получает телефонную книгу из c++ части
-        val phonebook = getPhonebook()
-
-        binding.textViewTopName.text = phonebook[0].name
-        binding.textViewTopNumber.text = phonebook[0].number
+        binding.textViewTopName.text = mainViewModel.phonebook[0].name
+        binding.textViewTopNumber.text = mainViewModel.phonebook[0].number
 
         // запоминает id последнего добавленного элемента чтобы прикреплять к нему последующий
         var previousNameViewId = binding.textViewTopName.id
@@ -38,18 +41,18 @@ class MainActivity : AppCompatActivity() {
             resources.displayMetrics
         ).toInt()
 
-        for (i in 1 until phonebook.size) {
+        for (i in 1 until mainViewModel.phonebook.size) {
 
             previousNameViewId = addTextView(
                 true,
-                phonebook[i].name,
+                mainViewModel.phonebook[i].name,
                 previousNameViewId,
                 padding
             )
 
             previousNumberViewId = addTextView(
                 false,
-                phonebook[i].number,
+                mainViewModel.phonebook[i].number,
                 previousNumberViewId,
                 padding
             )
@@ -102,6 +105,15 @@ class MainActivity : AppCompatActivity() {
         init {
             System.loadLibrary("testapp")
         }
+    }
+}
+
+class MainViewModel(val phonebook: Array<PhonebookEntry>) : ViewModel()
+
+class MainViewModelFactory(private val phonebook: Array<PhonebookEntry>) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return modelClass.getConstructor(Array<PhonebookEntry>::class.java).newInstance(phonebook)
     }
 }
 
